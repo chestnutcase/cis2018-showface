c49c1928bccf8e2747804e197396c9fcd7fd0abc
Tong Hui Kang <tonghuikang@outlook.com>
2018-09-22 17:24:58 +0800

    tetris square

---
+++ codeitsuisse/routes/tetris.py
@@ -0,0 +1,25 @@
+import logging
+
+from flask import request, jsonify;
+import requests
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+def tetris(jsoninput):
+    seq = list(jsoninput["tetrominoSequence"])
+    
+    for t in seq:
+        solution.append(counter%10)
+        counter += 2
+        
+    return {"actions":solution}
+    
+
+@app.route('/tetris', methods=['POST'])
+def evaluate_air():
+    data = request.get_json();
+    requests.post('http://requestbin.fullcontact.com/1k953nu1', json=data);
+    logging.info("data sent for evaluation {}".format(data));
+    output = tetris(data);
+    return jsonify(output);
