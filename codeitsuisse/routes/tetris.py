c9def91bdb3305c8392334626a1c927c4ee6bd27
chesnutcase <chester8991@gmail.com>
2018-09-22 23:33:21 +0800

    add tetris

--- codeitsuisse/routes/tetris.py
+++ codeitsuisse/routes/tetris.py
@@ -8,16 +8,16 @@ logger = logging.getLogger(__name__)
 
 def tetris(jsoninput):
     seq = list(jsoninput["tetrominoSequence"])
-    
+
     for t in seq:
         solution.append(counter%10)
         counter += 2
-        
+
     return {"actions":solution}
-    
+
 
 @app.route('/tetris', methods=['POST'])
-def evaluate_air():
+def evaluate_tetris():
     data = request.get_json();
     requests.post('http://requestbin.fullcontact.com/1k953nu1', json=data);
     logging.info("data sent for evaluation {}".format(data));
