13f5a9452d97abecc289080c6c3c772bcc7a4bb7
chesnutcase <chester8991@gmail.com>
2018-09-23 10:32:54 +0800

    silence sortinggame

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -11,4 +11,4 @@ import codeitsuisse.routes.photogps;
 import codeitsuisse.routes.DLQ2;
 import codeitsuisse.routes.broadcaster_debugged;
 import codeitsuisse.routes.tetris;
-import codeitsuisse.routes.sortinggame;
+#import codeitsuisse.routes.sortinggame;
